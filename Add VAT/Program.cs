be2b9e5d68f0ed704fb351cc7a1b45cbc530d210
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2021-02-02 22:05:03 +0100

    1st

---
+++ Add VAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] numbers = Console.ReadLine().Split(", ")
+                .Select(decimal.Parse)
+                .Select(number => number * 1.20m)
+                .ToArray();
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number:F2}");
+            }
+        }
+    }
+}
---
+++ Sum Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSumAndCount(int.Parse, array => array.Length, array => array.Sum());
+            Func<string, int> parser = x => int.Parse(x);
+        }
+
+        static void PrintSumAndCount(Func<string, int> parser,
+                                     Func<int[], int> CountGetter,
+                                     Func<int[], int> sumCalculator)
+        {
+            int[] array = Console.ReadLine().Split(", ")
+                 .Select(parser)
+                 .ToArray();
+            Console.WriteLine(CountGetter(array));
+            Console.WriteLine(sumCalculator(array));
+        }
+    }
+}
